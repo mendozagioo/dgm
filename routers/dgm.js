e6b3a118770881bcd7888b1924591af4124405c9
KielRodriguez <kr.rdz.20@gmail.com>
2018-05-23 15:40:49 -0500

    rename sitemap.xml to dgm.xml, create sitemap root

--- routers/dgm.js
+++ routers/dgm.js
@@ -8,20 +8,25 @@ var router = express();
 
 router.get('/', function(req, res){
     var urls = [
-        { url: '/',  changefreq: 'never', priority: 0.7 },
-        { url: '/blog/',  changefreq: 'never', priority: 0.7 },
-        { url: '/herramientas/',  changefreq: 'never', priority: 0.7 },
-        { url: '/desarrolladores/',  changefreq: 'never', priority: 0.7 }
+        { url: '/',  changefreq: 'never', priority: 0.7, lastmodISO: '2016-06-22T00:00:00-00:00' },
+        { url: '/blog/',  changefreq: 'never', priority: 0.7, lastmodISO: '2016-06-22T00:00:00-00:00' },
+        { url: '/herramientas/',  changefreq: 'never', priority: 0.7, lastmodISO: '2016-06-22T00:00:00-00:00' },
+        { url: '/desarrolladores/',  changefreq: 'never', priority: 0.7, lastmodISO: '2016-06-22T00:00:00-00:00' }
     ];
 
     Post.find({status: 'PUBLISHED'})
     .populate('section')
     .sort('-section')
-    .select('slug section')
+    .select('slug section creation_date')
     .then((posts) => {
         posts.forEach( post => {
             if ( post.section.slug == 'blog' || post.section.slug == 'herramientas' || post.section.slug == 'api-cdn') { 
-                urls.push({ url: '/' + post.section.slug + '/' + post.slug,  changefreq: 'never', priority: 0.5 });
+                urls.push({ 
+                    url: '/' + post.section.slug + '/' + post.slug,  
+                    changefreq: 'never', 
+                    priority: 0.5,
+                    lastmodISO: post.creation_date
+                });
             }
         });
 
