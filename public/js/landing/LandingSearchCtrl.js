d69fe05388ac5fa4f5579810cca164bea7c780ce
Edson Amaya <edsonjab@gmail.com>
2018-05-30 14:23:55 -0500

    Se agrega funcion para reemplazar acentos por código ASCII para que funcione en IE

--- public/js/landing/LandingSearchCtrl.js
+++ public/js/landing/LandingSearchCtrl.js
@@ -28,6 +28,20 @@ define( function () {
             window.open( 'busca/dataset?' + query, '_self' );
         };
 
+        $scope.ignoreAccents = function(item) {          
+            var text = $scope.removeAccents(item.toString()); 
+            window.location.href = "https://datos.gob.mx/busca/dataset?theme="+text;
+        };
+
+        $scope.removeAccents = function(value) {
+          return value.toString()
+              .replace(/á/g, '%C3%A1')    
+              .replace(/é/g, '%C3%A9')
+              .replace(/í/g, '%C3%AD')
+              .replace(/ó/g, '%C3%B3')
+              .replace(/ú/g, '%C3%BA');
+        };
+
         $http.get('https://api.datos.gob.mx/v2/resources?pageSize=1')
         .then(function(response){
           if( response.data.pagination != undefined )
@@ -40,4 +54,7 @@ define( function () {
             $scope.numberOrganizations = response.data.pagination.total;
         });
     };
+
+     
 });
+
